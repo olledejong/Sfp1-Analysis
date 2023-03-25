20390c5a91179ca663aed632d655360d546543b6
Olle de Jong <olledejong@gmail.com>
2023-03-25 15:10:28 +0100

    Created script that is the optimized version of the Jupyter Notebook analysis.

---
+++ volume_analysis_script.py
@@ -0,0 +1,278 @@
+import os
+import random
+import re
+from math import pi, sin, cos
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy.ndimage import binary_erosion
+from skimage.io import imread
+from skimage.filters import threshold_local, threshold_minimum, threshold_multiotsu
+from skimage.morphology import remove_small_objects, disk, erosion
+import bottleneck as bn
+
+plt.style.use('seaborn-v0_8')
+
+#######################
+### IMPORTANT PATHS ###
+#######################
+data_dir = "C:/Users/Olle de Jong/Documents/MSc Biology/MSB Research/Code and data/Data"
+tiff_files_dir = data_dir + "/processed_tiffs_nup133/"  # relative path from data directory to tiff directory
+output_dir = data_dir + "/Output/"  # relative path from data directory to image output directory
+budj_data_folder = data_dir + "/Other/Nup133/BudJ/"  # folder that holds the BudJ info on all cells
+budding_data_path = data_dir + "/Other/Nup133/buddings.txt"  # budding events
+kario_data_path = data_dir + "/Other/Nup133/kariokinesis.txt"  # kariokinesis events
+
+############################
+### THRESHOLDING GLOBALS ###
+############################
+scaling_factor = 0.16  # microns per pixel ---> 100x objective
+bloc_size_frac_to_use = 0.09  # fraction of the total cell mask pixels that is used as local thresholding block size
+offset_to_use = -50  # offset for local thresholding
+
+
+#################
+### FUNCTIONS ###
+#################
+def ellipse_from_budj(t, cell_data):
+    """
+    Define the function that can extract parameters of the ellipse using the data
+    Function to extract parameters of the ellipse from the BudJ table
+    """
+    data_at_frame = cell_data[cell_data["TimeID"] == t]
+    x_pos = float(data_at_frame['x']) / scaling_factor
+    y_pos = float(data_at_frame['y']) / scaling_factor
+    majorR = float(data_at_frame["Major R"]) / scaling_factor
+    minorR = float(data_at_frame["Minor r"]) / scaling_factor
+    angle = float(data_at_frame['Angle']) * (pi / 180)  # convert to radians
+    return x_pos, y_pos, majorR, minorR, angle
+
+
+def get_ellipse(imageGFP, nuc_mask):
+    """
+    Function that generates and fits the ellipse using the opencv package.
+    :param imageGFP: the
+    :param nuc_mask: nuclear mask determined by budj data
+    :return:
+    """
+    mask = imageGFP * nuc_mask
+    thresh = mask.astype(np.uint8)  # change type to uint8
+
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+    # if there is a contour found
+    if len(contours) > 0:
+        cnt = max(contours,key=len)
+        if len(cnt) > 4:
+            return cv2.fitEllipse(cnt)
+
+
+def round_up_to_odd(f):
+    """
+    Rounds up any number to a whole odd integer
+    """
+    return int(np.ceil(f) // 2 * 2 + 1)
+
+
+def load_budj_files():
+    # find all the files that hold budj data
+    files = []
+    for filename in os.listdir(budj_data_folder):
+        if ".csv" in filename:
+            prefix_position = f"pos{filename[-25:-23]}_"
+            files.append((filename, prefix_position))
+    return files
+
+
+def load_all_budj_data():
+    """
+    Collects all file names and using that, all BudJ data is loaded from the files
+    :return:
+    """
+    files = load_budj_files()  # get the separate files
+
+    # convert time_ids to real time minutes
+    TimeIDs = range(1, 151)  # TimeIDs range 1 through 150
+    time_conversion = pd.DataFrame({
+        "TimeID": TimeIDs,
+        "Time": [x * 5 for x in TimeIDs]
+    })
+
+    # add the data of all files to one single dataframe
+    budj_data = pd.DataFrame({})
+    for f in files:
+        pos_data = pd.read_csv(budj_data_folder + f[0], header=0, index_col=0)
+        pos_data["Cell_pos"] = f[1] + pos_data["Cell"].map(str)
+        pos_data = pos_data.loc[:, ["TimeID", "Cell_pos", "Volume", "x", "y", "Major R", "Minor r", "Angle"]]
+        budj_data = pd.concat([budj_data, pos_data])
+
+    budj_data = pd.merge(budj_data, time_conversion, on="TimeID")
+    budj_data = budj_data.sort_values(["Cell_pos", "TimeID"])
+    return budj_data
+
+
+def load_events():
+    """
+    Using BudJ, the kyrokinesis and budding events have been tracked. This function loads that
+    data and stores it in a dictionary object.
+    :return:
+    """
+    events_files = {"budding": budding_data_path, "kariokinesis": kario_data_path}
+
+    budding_events = {}
+    kario_events = {}
+    for event_type in events_files:  # for either budding or kariokinesis events
+        opened_file = open(events_files[event_type])  # open the file
+        events = {}  # temp storage object
+        for line in opened_file:  # every line in the file is a cell
+            if line == "\n":
+                continue
+            # process the two parts of the line by removing characters
+            parts = line.split(':')
+            cell_id = parts[0].replace("\"", "").strip()  # first part of the line is the individual cell
+            timepoints = re.sub('[\[\]]', "", parts[1])  # second part are the timepoints
+            # split timepoints on space to capture them in a list
+            split_timepoints = timepoints.split(",")
+            events[cell_id] = [int(x.strip()) for x in split_timepoints[:-1]]  # remove redundant last comma
+
+        # save the events to the right dictionary
+        if event_type == "budding":
+            budding_events = events
+        if event_type == "kariokinesis":
+            kario_events = events
+        opened_file.close()
+    return budding_events, kario_events
+
+
+def get_whole_cell_mask(t, single_cell_data, image_shape):
+    """
+    Given a certain time-point in the tiff movie, the single cell BudJ data, and the image shape,
+    the area of the cell is calculated.
+    :param t:
+    :param single_cell_data:
+    :param image_shape:
+    :return: whole_cell_mask, the x pos and the y pos of the cell
+    """
+    # get/calculate the ellipse information
+    x_pos, y_pos, majorR, minorR, A = ellipse_from_budj(t, single_cell_data)
+
+    y_dim, x_dim = image_shape  # get the dimensions (512x512)
+
+    # create an ogrid that helps us select/'mask' the info we want
+    row, col = np.ogrid[:y_dim, :x_dim]
+
+    # get the mask of the whole cell
+    whole_cell_mask = (
+            (
+                    (((col - x_pos) * cos(A) + (row - y_pos) * sin(A)) ** 2) / (majorR ** 2)
+                    +
+                    (((col - x_pos) * sin(A) - (row - y_pos) * cos(A)) ** 2) / (minorR ** 2) - 1
+            )  # if this sum
+            < 0  # is smaller than zero
+    )
+    return whole_cell_mask, x_pos, y_pos
+
+
+def main():
+    budj_data = load_all_budj_data()  # load the budj data from all the separate files
+    budding_events, kario_events = load_events()  # load the kyrokinesis and budding events
+    individual_cells = sorted(list(set(budj_data["Cell_pos"])))  # how many cells are there in total
+
+    final_dataframe = pd.DataFrame({})
+
+    for pos in range(1, 21):
+        if pos < 10:
+            pos = "0" + str(pos)
+        pos = str(pos)
+        image = imread(os.path.join(f"{tiff_files_dir}2022_12_06_nup133_yegfp_xy{pos}.nd2.tif"))  # load the image
+        imageGFP = image[:, 1, :, :]  # get the GFP data
+
+        for cell in [i for i in individual_cells if pos in i]:
+            could_not_fit = 0
+
+            single_cell_data = budj_data[budj_data["Cell_pos"] == cell]  # get the data for the single cell
+            single_cell_data = single_cell_data.sort_values(by="Time")
+
+            cell_areas = []
+            nuc_areas = []
+            cell_volumes = []
+            nuc_volumes = []
+            for t in single_cell_data.TimeID:
+                t_in_tiff = t - 1  # skew the time by one for tiff dataframe
+                imageGFP_at_frame = imageGFP[t_in_tiff, :, :]  # get the GFP data
+
+                # get the cell mask through budj data
+                whole_cell_mask, x_pos, y_pos = get_whole_cell_mask(t, single_cell_data,  imageGFP_at_frame.shape)
+                imageGFP_cell_mask = imageGFP_at_frame * whole_cell_mask  # keep only cell data
+
+                num_cell_pixels = np.count_nonzero(whole_cell_mask == True)
+                bloc_size_cell_size_dependent = round_up_to_odd(bloc_size_frac_to_use * num_cell_pixels)
+
+                nucl_thresh_mask_local = threshold_local(
+                    image=imageGFP_cell_mask,
+                    block_size=bloc_size_cell_size_dependent,
+                    offset=offset_to_use
+                )
+                imageGFP_nuc_mask_local = remove_small_objects(imageGFP_cell_mask > nucl_thresh_mask_local)
+
+                # fit an ellipse on the whole cell mask
+                (c_x, c_y), (c_MA, c_ma), c_angle = get_ellipse(imageGFP_at_frame, whole_cell_mask)
+
+                r1c = (c_MA * scaling_factor)  # to get the semi-major axis of the whole cell
+                r2c = (c_ma * scaling_factor)  # to get the semi-minor axis of the whole cell
+                r3c = (np.average((c_MA, c_ma)) * scaling_factor)  # to get the third axis
+                cell_volume = (4 / 3) * pi * r1c * r2c * r3c
+                cell_area = pi * r1c * r2c
+
+                cell_areas.append(cell_area)  # save the area
+                cell_volumes.append(cell_volume)  # save the volume for this datapoint
+
+                # try to fit the ellipse on the nucleus, if there is none, then add None to the list
+                try:
+                    (x, y), (MA, ma), angle = get_ellipse(imageGFP_at_frame, imageGFP_nuc_mask_local)
+                    cv2_ellipse = ((x, y), (MA, ma), angle)
+                except Exception:
+                    # thresholding did not lead to an ellipse, nucleus was probably out of focus
+                    could_not_fit += 1
+                    nuc_volumes.append(None)
+                    continue
+
+                r1n = (MA * scaling_factor)  # to get the semi-major axis of the nucleus
+                r2n = (ma * scaling_factor)  # to get the semi-minor axis of the nucleus
+                r3n = (np.average((MA, ma)) * scaling_factor)  # to get the third axis
+                nuc_volume = (4 / 3) * pi * r1n * r2n * r3n
+                nuc_area = pi * r1n * r2n
+
+                # save this time-point its area and volume of the nucleus
+                nuc_areas.append(nuc_area)
+                nuc_volumes.append(nuc_volume)
+
+            # calculate the ratios
+            ratios = []
+            i = 0
+            for nuc_vol in nuc_volumes:
+                if nuc_vol is None:
+                    ratios.append(None)
+                else:
+                    ratios.append(nuc_vol / cell_volumes[i])
+
+            # store data in this cell its dataframe
+            single_cell_data["Cell_volume"] = pd.Series(cell_volumes, index=single_cell_data.index, dtype="float64")
+            single_cell_data["Nucleus_volume"] = pd.Series(nuc_volumes, index=single_cell_data.index, dtype="float64")
+            single_cell_data["N/C_ratio"] = pd.Series(ratios, index=single_cell_data.index, dtype="float64")
+
+            # store this cell's data in the final dataframe which eventually contains data of all cells
+            final_dataframe = pd.concat([final_dataframe, single_cell_data])
+
+            print(f"{cell} - Couldn't fit ellipse {could_not_fit} out of {len(single_cell_data['TimeID'])} times")
+
+    final_dataframe = final_dataframe.reset_index(drop=True)  # reset index of dataframe
+    final_dataframe.to_excel(f"{output_dir}excel/nup133_final_data_script_test.xlsx")  # save the final file
+    print("Done.")
+
+
+# SCRIPT STARTS HERE
+if __name__ == "__main__":
+    main()
