65ef9f4bd3c4cb2656aed58bf18ee231bdd88462
Olle de Jong <olledejong@gmail.com>
2023-03-27 10:43:30 +0200

    Added interpolation part of volume_analysis.ipynb to volume_analysis_script.py.

--- volume_analysis_script.py
+++ volume_analysis_script.py
@@ -1,5 +1,4 @@
 import os
-import random
 import re
 from math import pi, sin, cos
 import pandas as pd
@@ -11,7 +10,6 @@ from scipy.ndimage import binary_erosion
 from skimage.io import imread
 from skimage.filters import threshold_local, threshold_minimum, threshold_multiotsu
 from skimage.morphology import remove_small_objects, disk, erosion
-import bottleneck as bn
 
 plt.style.use('seaborn-v0_8')
 
@@ -53,7 +51,7 @@ def ellipse_from_budj(t, cell_data):
 def get_ellipse(imageGFP, nuc_mask):
     """
     Function that generates and fits the ellipse using the opencv package.
-    :param imageGFP: the
+    :param imageGFP: the GFP image data
     :param nuc_mask: nuclear mask determined by budj data
     :return:
     """
@@ -64,7 +62,7 @@ def get_ellipse(imageGFP, nuc_mask):
 
     # if there is a contour found
     if len(contours) > 0:
-        cnt = max(contours,key=len)
+        cnt = max(contours, key=len)
         if len(cnt) > 4:
             return cv2.fitEllipse(cnt)
 
@@ -146,6 +144,21 @@ def load_events():
     return budding_events, kario_events
 
 
+def get_area_and_vol(minor_r, major_r):
+    """
+    Calculates the area and the volume of an ellipse using the major and minor radii
+    :param minor_r:
+    :param major_r:
+    :return:
+    """
+    r1c = (major_r * scaling_factor)  # to get the semi-major axis of the whole cell
+    r2c = (minor_r * scaling_factor)  # to get the semi-minor axis of the whole cell
+    r3c = (np.average((major_r, minor_r)) * scaling_factor)  # to get the third axis
+    volume = (4 / 3) * pi * r1c * r2c * r3c
+    area = pi * r1c * r2c
+    return area, volume
+
+
 def get_whole_cell_mask(t, single_cell_data, image_shape):
     """
     Given a certain time-point in the tiff movie, the single cell BudJ data, and the image shape,
@@ -175,13 +188,14 @@ def get_whole_cell_mask(t, single_cell_data, image_shape):
     return whole_cell_mask, x_pos, y_pos
 
 
-def main():
-    budj_data = load_all_budj_data()  # load the budj data from all the separate files
-    budding_events, kario_events = load_events()  # load the kyrokinesis and budding events
-    individual_cells = sorted(list(set(budj_data["Cell_pos"])))  # how many cells are there in total
+def get_volume_data(budj_data, individual_cells):
+    """
 
+    :param budj_data:
+    :param individual_cells:
+    :return:
+    """
     final_dataframe = pd.DataFrame({})
-
     for pos in range(1, 21):
         if pos < 10:
             pos = "0" + str(pos)
@@ -190,21 +204,16 @@ def main():
         imageGFP = image[:, 1, :, :]  # get the GFP data
 
         for cell in [i for i in individual_cells if pos in i]:
+            single_cell_data = budj_data[budj_data["Cell_pos"] == cell].sort_values(by="Time")  # get single cell data
             could_not_fit = 0
 
-            single_cell_data = budj_data[budj_data["Cell_pos"] == cell]  # get the data for the single cell
-            single_cell_data = single_cell_data.sort_values(by="Time")
-
-            cell_areas = []
-            nuc_areas = []
-            cell_volumes = []
-            nuc_volumes = []
+            cell_areas, nuc_areas, cell_volumes, nuc_volumes = [], [], [], []
             for t in single_cell_data.TimeID:
                 t_in_tiff = t - 1  # skew the time by one for tiff dataframe
                 imageGFP_at_frame = imageGFP[t_in_tiff, :, :]  # get the GFP data
 
                 # get the cell mask through budj data
-                whole_cell_mask, x_pos, y_pos = get_whole_cell_mask(t, single_cell_data,  imageGFP_at_frame.shape)
+                whole_cell_mask, x_pos, y_pos = get_whole_cell_mask(t, single_cell_data, imageGFP_at_frame.shape)
                 imageGFP_cell_mask = imageGFP_at_frame * whole_cell_mask  # keep only cell data
 
                 num_cell_pixels = np.count_nonzero(whole_cell_mask == True)
@@ -217,59 +226,103 @@ def main():
                 )
                 imageGFP_nuc_mask_local = remove_small_objects(imageGFP_cell_mask > nucl_thresh_mask_local)
 
-                # fit an ellipse on the whole cell mask
-                (c_x, c_y), (c_MA, c_ma), c_angle = get_ellipse(imageGFP_at_frame, whole_cell_mask)
-
-                r1c = (c_MA * scaling_factor)  # to get the semi-major axis of the whole cell
-                r2c = (c_ma * scaling_factor)  # to get the semi-minor axis of the whole cell
-                r3c = (np.average((c_MA, c_ma)) * scaling_factor)  # to get the third axis
-                cell_volume = (4 / 3) * pi * r1c * r2c * r3c
-                cell_area = pi * r1c * r2c
-
-                cell_areas.append(cell_area)  # save the area
-                cell_volumes.append(cell_volume)  # save the volume for this datapoint
+                # save cell area and volume to dataframe
+                (c_x, c_y), (c_MA, c_ma), c_angle = get_ellipse(imageGFP_at_frame, whole_cell_mask)  # fit ellipse cell
+                cell_area, cell_volume = get_area_and_vol(c_ma, c_MA)  # get the area and volume of the cell
+                cell_areas.append(cell_area)
+                cell_volumes.append(cell_volume)
 
                 # try to fit the ellipse on the nucleus, if there is none, then add None to the list
                 try:
                     (x, y), (MA, ma), angle = get_ellipse(imageGFP_at_frame, imageGFP_nuc_mask_local)
-                    cv2_ellipse = ((x, y), (MA, ma), angle)
                 except Exception:
                     # thresholding did not lead to an ellipse, nucleus was probably out of focus
                     could_not_fit += 1
                     nuc_volumes.append(None)
                     continue
 
-                r1n = (MA * scaling_factor)  # to get the semi-major axis of the nucleus
-                r2n = (ma * scaling_factor)  # to get the semi-minor axis of the nucleus
-                r3n = (np.average((MA, ma)) * scaling_factor)  # to get the third axis
-                nuc_volume = (4 / 3) * pi * r1n * r2n * r3n
-                nuc_area = pi * r1n * r2n
-
-                # save this time-point its area and volume of the nucleus
+                # save nucleus area and volume to dataframe
+                nuc_area, nuc_volume = get_area_and_vol(ma, MA)
                 nuc_areas.append(nuc_area)
                 nuc_volumes.append(nuc_volume)
 
             # calculate the ratios
             ratios = []
-            i = 0
-            for nuc_vol in nuc_volumes:
-                if nuc_vol is None:
-                    ratios.append(None)
-                else:
-                    ratios.append(nuc_vol / cell_volumes[i])
+            for i in range(0, len(nuc_volumes)):
+                ratios.append(None) if nuc_volumes[i] is None else ratios.append(nuc_volumes[i] / cell_volumes[i])
 
             # store data in this cell its dataframe
+            single_cell_data["Cell_area"] = pd.Series(cell_areas, index=single_cell_data.index, dtype="float64")
             single_cell_data["Cell_volume"] = pd.Series(cell_volumes, index=single_cell_data.index, dtype="float64")
+            single_cell_data["Nucleus_area"] = pd.Series(nuc_areas, index=single_cell_data.index, dtype="float64")
             single_cell_data["Nucleus_volume"] = pd.Series(nuc_volumes, index=single_cell_data.index, dtype="float64")
             single_cell_data["N/C_ratio"] = pd.Series(ratios, index=single_cell_data.index, dtype="float64")
 
             # store this cell's data in the final dataframe which eventually contains data of all cells
             final_dataframe = pd.concat([final_dataframe, single_cell_data])
-
             print(f"{cell} - Couldn't fit ellipse {could_not_fit} out of {len(single_cell_data['TimeID'])} times")
 
+    # remove unrealistic N/C ratios
+    final_dataframe = final_dataframe.drop(final_dataframe[final_dataframe["N/C_ratio"] > .32].index)
     final_dataframe = final_dataframe.reset_index(drop=True)  # reset index of dataframe
-    final_dataframe.to_excel(f"{output_dir}excel/nup133_final_data_script_test.xlsx")  # save the final file
+    final_dataframe.to_excel(f"{output_dir}excel/nup133_final_data_script_removed_outliers.xlsx")  # save the final file
+    return final_dataframe
+
+
+def split_cycles_and_interpolate(final_dataframe, individual_cells, kario_events):
+    """
+
+    :param final_dataframe:
+    :param individual_cells:
+    :param kario_events:
+    :return:
+    """
+    desired_datapoints = 100
+    cols = ["cell", "cycle", "start", "end"]
+    cols += range(desired_datapoints)
+    data_interpolated = pd.DataFrame(columns=cols)
+
+    for cell in individual_cells[1:]:  # remove cell pos01_1, since it has too many missing frames
+        single_cell_data = final_dataframe[final_dataframe.Cell_pos == cell]
+        single_cell_data = single_cell_data[~single_cell_data.Nucleus_volume.isnull()]
+
+        # print(f"Kario events for {cell}: {kario_events[cell]}")
+        count = 0
+        while count < len(kario_events[cell]) - 1:
+            tp1 = kario_events[cell][count]
+            tp2 = kario_events[cell][count + 1]
+            # print(f"{cell} - Getting N/C ratio data from {tp1} through {tp2}")
+            cell_cycle_dat = single_cell_data[single_cell_data.TimeID.between(tp1, tp2)]
+
+            # when there are not enough datapoints for this cycle, because of outlier cleaning, then ignore that cycle
+            if len(cell_cycle_dat) < 10:
+                count += 1
+                continue
+
+            # get the original x and y data
+            x = np.linspace(0, len(cell_cycle_dat), len(cell_cycle_dat))
+            y = cell_cycle_dat['N/C_ratio']
+
+            # interpolate the data towards 100 datapoints
+            x_new = np.linspace(0, len(cell_cycle_dat), 100)
+            f1 = interpolate.interp1d(x, y, kind='linear')
+            f2 = interpolate.interp1d(x, y, kind='cubic')
+
+            # add the interpolated data to a dataframe
+            data_interpolated.loc[len(data_interpolated)] = [cell, count + 1, tp1, tp2] + f2(x_new).tolist()
+            count += 1
+
+    data_interpolated.to_excel(f"{output_dir}excel/cycles_interpolated_script_removed_outliers.xlsx")  # save the final file
+    return data_interpolated
+
+
+def main():
+    budj_data = load_all_budj_data()  # load the budj data from all the separate files
+    budding_events, kario_events = load_events()  # load the kyrokinesis and budding events
+    individual_cells = sorted(list(set(budj_data["Cell_pos"])))  # how many cells are there in total
+
+    final_dataframe = get_volume_data(budj_data, individual_cells)
+    interpolated_data = split_cycles_and_interpolate(final_dataframe, final_dataframe, kario_events)
     print("Done.")
 
 
